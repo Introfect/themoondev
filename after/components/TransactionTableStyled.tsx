3e12e41af58ad4efbe552d33b35826c5479ba831
Aayushman Pratap <76990626+Introfect@users.noreply.github.com>
2023-12-28 20:05:05 +0530

    added components

---
+++ after/components/TransactionTableStyled.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+type Props = {}
+
+function TransactionTableStyled({}: Props) {
+  return (
+    <div>
+          <div className="header">
+          <p className="header_label">Burn Transactions</p>
+        </div>
+        <BurnTxTable
+          data={burnTransactions}
+          priceUSD={coinData?.current_price?.usd}
+        />
+    </div>
+  )
+}
+
+export default TransactionTableStyled
